67737b3054034a6c7642f32f0e5c3e5049630d00
李照闫 <2236368544@qq.com>
2017-09-25 14:41:34 +0800

    Add the basic project files of the demo

---
+++ AAProgressBarDemo/AAProgressBarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AAProgressBarDemo
+//
+//  Created by An An on 2017/9/25.
+//  Copyright © 2017年 An An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AAProgressBarDemo/AAProgressBarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AAProgressBarDemo
+//
+//  Created by An An on 2017/9/25.
+//  Copyright © 2017年 An An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
